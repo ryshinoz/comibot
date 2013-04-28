905dcfc25538f5d6d2f12a1c61415cc80535b18a
Ryosuke Shinozaki <ryshinoz@gmail.com>
2013-04-29 03:16:41 +0900

    ちょっと修正

--- lib/comibot/browser.rb
+++ lib/comibot/browser.rb
@@ -1,29 +1,34 @@
 module Comibot
   class Browser
 
-    attr_accessor :url
-
-    def initialize(url, meeting, email, password)
+    def initialize(url, timeout)
       @url      = url
-      @meeting  = meeting
-      @email    = email
-      @password = password
       @session  = Selenium::WebDriver.for :chrome
       @session.navigate.to @url
+      @wait = Selenium::WebDriver::Wait.new(:timeout => timeout)
     end
 
-    def login
-      @session.find_element(:id, 'user_email').send_keys @email
-      @session.find_element(:id, 'user_password').send_keys @password
+    def login(email, password)
+      @session.find_element(:id, 'user_email').send_keys email
+      @session.find_element(:id, 'user_password').send_keys password
       @session.find_element(:id, 'user_submit').click
     end
 
-    def new_message(message)
-      wait = Selenium::WebDriver::Wait.new(:timeout => 10)
-      wait.until {
-        @session.find_element(:xpath, "//a[@title='#{@meeting}']").click
+    def select_group(group_id)
+      @wait.until {
+        @session.find_element(:id, 'my-groups-button').click
       }
-      wait.until {
+      @session.find_element(:xpath, "//div[@id='my-groups-container']/ul[@id='my-groups']/li/a[@href='#{group_id}']").click
+    end
+
+    def select_meeting(meeting_name)
+      @wait.until {
+        @session.find_element(:xpath, "//a[@title='#{meeting_name}']").click
+      }
+    end
+
+    def new_message(message)
+      @wait.until {
         @session.find_element(:xpath, '//div[@class="SWCEY replyBox" and @kind="rb"]').click
       }
 
--- lib/comibot/message.rb
+++ lib/comibot/message.rb
@@ -1,8 +1,10 @@
 module Comibot
   class Message
-    def self.run(url, meeting, email, password, message)
-      @browser = Browser.new(url, meeting, email, password)
-      @browser.login
+    def self.run(url, email, password, group_id, meeting_name, message)
+      @browser = Browser.new(url, 10)
+      @browser.login(email, password)
+      @browser.select_group(group_id)
+      @browser.select_meeting(meeting_name)
       @browser.new_message(message)
       sleep(3)
       @browser.close
